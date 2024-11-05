01557a8ba70b9157e43dcec0a215638ea8a437c2
dean.ryan <L00188387@atu.ie>
2024-11-05 18:54:18 +0000

    test(common): add two tests for testing car functionality #1

---
+++ Assets/Tests/CarTest.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+public class CarTest
+{
+    [Test]
+    public void GetNoDoors_Test()
+    {
+        // Assuming GetNoDoors is a static property or method
+        int j = Car.GetNoDoors;
+
+        // Correct assertion to compare integers
+        Assert.AreEqual(1, j);
+    }
+
+    [Test]
+    public void SetNoDoors_Test()
+    {
+        // Assuming SetNoDoors is a static method that sets the number of doors
+        Car.SetNoDoors(2);
+
+        // Check if the number of doors was correctly set
+        int j = Car.GetNoDoors;
+        Assert.AreEqual(2, j);
+    }
+}
